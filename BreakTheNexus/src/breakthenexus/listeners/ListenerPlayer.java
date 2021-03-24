4568d02c8a1eb23c4339f73d3dd0b3062ccfe412
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-23 21:13:53 -0400

    Made players respawn in correct world

--- BreakTheNexus/src/breakthenexus/listeners/ListenerPlayer.java
+++ BreakTheNexus/src/breakthenexus/listeners/ListenerPlayer.java
@@ -4,6 +4,7 @@ import breakthenexus.BreakTheNexus;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class ListenerPlayer implements Listener {
 
@@ -11,6 +12,15 @@ public class ListenerPlayer implements Listener {
     public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
 
         playerJoinEvent.getPlayer().teleport(BreakTheNexus.getInstance().getMapLobby().getWorld().getSpawnLocation());
+
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent playerRespawnEvent) {
+
+        if (BreakTheNexus.getInstance().getMapLobby() != null) {
+            playerRespawnEvent.setRespawnLocation(BreakTheNexus.getInstance().getMapLobby().getWorld().getSpawnLocation());
+        }
 
     }
 
