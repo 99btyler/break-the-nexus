71203b83246f37694786ebb1592867de0089ffb8
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-25 21:15:03 -0400

    Stopped player hunger and damage in lobby

--- BreakTheNexus/src/breakthenexus/game/details/listeners/ListenerPlayer.java
+++ BreakTheNexus/src/breakthenexus/game/details/listeners/ListenerPlayer.java
@@ -1,8 +1,11 @@
 package breakthenexus.game.details.listeners;
 
 import breakthenexus.BreakTheNexus;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -16,6 +19,32 @@ public class ListenerPlayer implements Listener {
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent playerRespawnEvent) {
         playerRespawnEvent.setRespawnLocation(BreakTheNexus.getInstance().getPlaceToSpawn(playerRespawnEvent.getPlayer().getName()));
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent entityDamageEvent) {
+
+        if (!(entityDamageEvent.getEntity() instanceof Player)) {
+            return;
+        }
+
+        if (entityDamageEvent.getEntity().getWorld() == BreakTheNexus.getInstance().getMapLobby().getWorld()) {
+
+            entityDamageEvent.setCancelled(true);
+
+            if (entityDamageEvent.getCause() == EntityDamageEvent.DamageCause.VOID) {
+                entityDamageEvent.getEntity().teleport(BreakTheNexus.getInstance().getPlaceToSpawn(entityDamageEvent.getEntity().getName()));
+            }
+
+        }
+
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent foodLevelChangeEvent) {
+        if (foodLevelChangeEvent.getEntity().getWorld() == BreakTheNexus.getInstance().getMapLobby().getWorld()) {
+            foodLevelChangeEvent.setCancelled(true);
+        }
     }
 
 }
