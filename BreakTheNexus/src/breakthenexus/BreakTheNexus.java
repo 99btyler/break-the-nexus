8f6c516c42afe1eb68ee68af7ccba8bf9fad2fd1
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-31 16:17:59 -0400

    Cleaned up BreakTheNexus.java by removing print messages

--- BreakTheNexus/src/breakthenexus/BreakTheNexus.java
+++ BreakTheNexus/src/breakthenexus/BreakTheNexus.java
@@ -14,7 +14,6 @@ import breakthenexus.game.kit.kits.Warrior;
 import breakthenexus.managers.KitManager;
 import breakthenexus.managers.MineManager;
 import breakthenexus.managers.TeamManager;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -23,13 +22,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BreakTheNexus extends JavaPlugin {
 
     private Map mapLobby;
-
     private Map mapGame;
 
     private TeamManager teamManager;
-
     private MineManager mineManager;
-
     private KitManager kitManager;
 
     private static BreakTheNexus instance;
@@ -38,21 +34,17 @@ public class BreakTheNexus extends JavaPlugin {
     public void onEnable() {
 
         instance = this;
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus being enabled...");
 
         mapLobby = new Map("The Lobby");
         mapLobby.loadWorld();
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus mapLobby loaded!");
 
         mapGame = new Map("Roastal");
         mapGame.loadWorld();
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus mapGame loaded!");
 
         teamManager = new TeamManager(new Team[] {
                 new Team("Red"),
                 new Team("Blue")
         });
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus teams loaded!");
 
         mineManager = new MineManager(new Mine[] {
                 new Mine(Material.IRON_ORE, 15),
@@ -61,31 +53,23 @@ public class BreakTheNexus extends JavaPlugin {
                 new Mine(Material.MELON_BLOCK, Material.MELON, 5),
                 new Mine(Material.LOG, Material.WOOD, 10)
         });
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus mines loaded!");
 
         kitManager = new KitManager(new Kit[] {
                 new Civilian(),
                 new Miner(),
                 new Warrior()
         });
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus kits loaded!");
 
         getCommand("kit").setExecutor(new CommandKit());
         getCommand("team").setExecutor(new CommandTeam());
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus commands registered!");
 
         getServer().getPluginManager().registerEvents(new ListenerPlayer(), this);
         getServer().getPluginManager().registerEvents(new ListenerWorld(), this);
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus listeners registered!");
-
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus enabled!");
 
     }
 
     @Override
     public void onDisable() {
-
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus being disabled...");
 
         for (Player player : mapGame.getWorld().getPlayers()) {
             player.getInventory().clear();
@@ -93,12 +77,8 @@ public class BreakTheNexus extends JavaPlugin {
             player.setHealth(20.0);
             player.setFoodLevel(20);
         }
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus mapGame players moved to mapLobby!");
 
         mapGame.unloadWorld();
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus mapGame unloaded");
-
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "BreakTheNexus disabled");
 
     }
 
