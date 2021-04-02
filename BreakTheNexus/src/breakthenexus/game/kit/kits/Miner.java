a3135a7c32d32b09a0f2bf391411d02c88224f35
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-02 19:48:36 -0400

    Miner now spawns with enchanted pickaxe

--- BreakTheNexus/src/breakthenexus/game/kit/kits/Miner.java
+++ BreakTheNexus/src/breakthenexus/game/kit/kits/Miner.java
@@ -2,6 +2,7 @@ package breakthenexus.game.kit.kits;
 
 import breakthenexus.game.kit.Kit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 public class Miner extends Kit {
@@ -21,8 +22,11 @@ public class Miner extends Kit {
     @Override
     protected void setToolItems() {
 
+        final ItemStack enchantedPickaxe = new ItemStack(Material.STONE_PICKAXE);
+        enchantedPickaxe.addEnchantment(Enchantment.DIG_SPEED, 1);
+
         toolItems = new ItemStack[] {
-                new ItemStack(Material.STONE_PICKAXE),
+                enchantedPickaxe,
                 new ItemStack(Material.WOOD_SWORD),
                 new ItemStack(Material.WOOD_AXE)
         };
