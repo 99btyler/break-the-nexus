a7aac1a8f79cd5014cb63c8a3cd70f124b17a41a
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-02 19:02:58 -0400

    Stopped creatures from spawning

--- BreakTheNexus/src/breakthenexus/game/details/listeners/ListenerWorld.java
+++ BreakTheNexus/src/breakthenexus/game/details/listeners/ListenerWorld.java
@@ -2,6 +2,7 @@ package breakthenexus.game.details.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class ListenerWorld implements Listener {
@@ -9,6 +10,11 @@ public class ListenerWorld implements Listener {
     @EventHandler
     private void onWeatherChange(WeatherChangeEvent weatherChangeEvent) {
         weatherChangeEvent.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onCreatureSpawn(CreatureSpawnEvent creatureSpawnEvent) {
+        creatureSpawnEvent.setCancelled(true);
     }
 
 }
